154f018b6c7a876b5d3b0fe790ead576fa715196
Tamajin <wild.jlq@gmail.com>
2023-01-11 16:05:56 +0100

    success

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apod } from './apod';
+import { NasaService } from './nasa.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nasa-api';
+  imgOfTheDay: string = '';
+
+  constructor(public nasaService: NasaService) {}
+
+  ngOnInit() {
+    this.getImageOfTheDay();
+  }
+
+  getImageOfTheDay(): void {
+    this.nasaService
+      .getImageOfTheDay()
+      .subscribe((data) => (this.imgOfTheDay = data));
+  }
 }
